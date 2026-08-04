c1f262fa99d99d650207da426bf6faa27a2c3d55
Aarogya Regmi <155275398+Reg21meme@users.noreply.github.com>
2026-08-04 17:39:59 -0500

    Time: 81 ms (18.33%) | Memory: 111.3 MB (41.36%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> m;
+        
+        for (int x : nums){
+            if(m.count(x)){ return true;}
+            m.insert(x);
+        }
+        return false;
+    }
+};
