2d3c100f7ce3ceb59d4779b9011fba057e9b9611
Aarogya Regmi <155275398+Reg21meme@users.noreply.github.com>
2026-08-19 19:43:58 -0500

    Time: 3 ms (50.36%) | Memory: 20 MB (8.36%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> freq;
+        vector<vector<int>> buckets(nums.size() + 1);
         vector<int> ans;
-        for(int x : nums){
-           freq[x]++;
+
+        for( int x : nums){
+            freq[x]++;
         }
 
-        for(int i = 0; i < k; i++){
-            int bestNum = 0, bestCount = 0;
 
-            for(auto&p : freq){
-                if(p.second > bestCount){
-                    bestCount = p.second;
-                    bestNum = p.first;
+        for(auto&p: freq){
+            buckets[p.second].push_back(p.first);
+        }
+
+        for(int i = buckets.size()-1; i > 0; i--){
+            for(int num : buckets[i]){
+                ans.push_back(num);
+                if(ans.size() == k){
+                    return ans;
                 }
             }
-            ans.push_back(bestNum);
-            freq.erase(bestNum);
         }
         return ans;
     }
